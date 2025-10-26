c0dc073679a167a725a0926bd4d794e18f1c6a18
Mahan <Mahan.fakhrzadeh@gmail.com>
2025-10-26 14:02:21 +0330

    last changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "../components/App/App";
 import Header from "../components/Header/Header";
 import Slider from "../components/Slider/Slider";
 import Sale from "../components/Sale/Sale";
@@ -12,7 +11,6 @@ import MostSale from "../components/mostSale/mostSale";
 import BestBrands from "../components/BestBrands/BestBrands";
 import Article from "../components/Article/Article";
 import Footer from "../components/Footer/Footer";
-
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=shopping_cart"
@@ -20,15 +18,15 @@ import Footer from "../components/Footer/Footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header/>
-    <Slider/>
-    <Sale/>
-    <Category/>
-    <New/>
-    <Info/>
-    <MostSale/>
-    {/* <BestBrands/>
-    <Article/> */}
-    <Footer/>
+    <Header />
+    <Slider />
+    <Sale />
+    <Category />
+    <New />
+    <Info />
+    <MostSale />
+    <BestBrands />
+    <Article />
+    <Footer />
   </StrictMode>
 );
